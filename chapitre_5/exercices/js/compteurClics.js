fb4af6faf636f59426c46acbb6a527a87a4ea354
HuePatrice <patrice1279@gmail.com>
2017-01-13 07:11:30 +0100

    Ajout 1e exercice

---
+++ chapitre_5/exercices/js/compteurClics.js
@@ -0,0 +1,12 @@
+var compteurClics = 0;
+
+function clic(){
+    compteurClics++;
+    document.getElementById('compteurClics').textContent = compteurClics;
+}
+
+document.getElementById('clic').addEventListener('click', clic);
+
+document.getElementById('desactiver').addEventListener('click', function(){
+    document.getElementById('clic').removeEventListener('click', clic);
+});
