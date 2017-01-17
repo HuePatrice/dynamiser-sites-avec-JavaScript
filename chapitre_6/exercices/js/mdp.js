fa6b9e6da5b9c836804aceefd5b44c3a2d17f23c
HuePatrice <patrice1279@gmail.com>
2017-01-17 14:59:39 +0100

    Ajout exercice 'Mots de passe'

---
+++ chapitre_6/exercices/js/mdp.js
@@ -0,0 +1,38 @@
+/*
+    A la validation du formulaire
+        verifier si les deux mdp sont identiques
+         doivent avoir au moins 6 caractères
+         doit contenir au moins 1 chiffre
+ */
+
+var form = document.querySelector('form');
+form.addEventListener('submit', function(e){
+
+    var mdp1 =  form.elements.mdp1.value;
+    var mdp2 =  form.elements.mdp2.value;
+
+    var mdpMsg="Mots de passe OK";
+
+    if(mdp1 === mdp2){
+       if(mdp1.length >= 6){
+
+           var regex = /\d+/;
+
+           if(!regex.test(mdp1)) {
+               mdpMsg = 'Votre mot de passe doit contenir au moins un chiffre';
+           }
+
+       }else{
+
+           mdpMsg = "La longueur du mot de passe doit être d'au minimum 6 caractères";
+
+       }
+
+    } else {
+        mdpMsg = 'Vos mots de passe sont différents';
+    }
+
+    document.getElementById('infoMdp').textContent = mdpMsg;
+    e.preventDefault();
+
+});
